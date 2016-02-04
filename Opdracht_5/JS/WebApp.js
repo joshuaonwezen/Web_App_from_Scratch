36e48b4fb99d1937dcfd255496a7a00318237d97
Raymond <raymondkorrel@hotmail.com>
2016-02-04 14:22:36 +0100

    review

--- Opdracht_5/JS/WebApp.js
+++ Opdracht_5/JS/WebApp.js
@@ -11,12 +11,12 @@
             init: function () { 
                 //Window onload to wait for objects to exist
                 window.onload = function () {
-                    if (window.location.href.indexOf('#') != -1) {
-                        webApp.sections.toggle(window.location.href)
+                    if (window.location.hash) {
+                        webApp.sections.toggle(window.location.hash)
                     }
                 }
                 window.addEventListener('hashchange', function(){
-                    webApp.sections.toggle(window.location.href);
+                    webApp.sections.toggle(window.location.hash);
                 });
             },
         },
@@ -44,3 +44,9 @@
     
     webApp.app.init();
 })();
+
+// Raymond's code review:
+// in de if else in routes.init functie check je met (window.location.href.indexOf('#') != -1) of er een hash in de url zit. 
+// Dit kan veel makkelijker door (window.location.hash) in te vullen in de if.
+// Je geeft de volledige url (window.location.href) door aan de toggle functie waarin je hem ontleedt.
+// Door simpelweg window.location.hash door te geven, kan je hem in één keer in de queryselector zetten.
