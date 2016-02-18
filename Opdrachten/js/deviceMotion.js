d88436fd2c6f6e5c9d4bfcae39fa255bf25a9f9f
Joshua Onwezen <joshua_onwezen@hotmail.com>
2016-02-18 22:10:40 +0100

    and we're done

--- Opdrachten/js/deviceMotion.js
+++ Opdrachten/js/deviceMotion.js
@@ -1,14 +1,7 @@
 var motionHandling = {
     //Source: http://www.html5rocks.com/en/tutorials/device/orientation/
     deviceMotionHandler: function (eventData) {
-        var gravity, xyz = "[X, Y, Z]";
-    
-        // Grab the acceleration including gravity from the results
         var acceleration = eventData.accelerationIncludingGravity;
-        gravity = xyz.replace("X", acceleration.x);
-        gravity = gravity.replace("Y", acceleration.y);
-        gravity = gravity.replace("Z", acceleration.z);
-        //document.getElementById("gravity").innerHTML = gravity;
         
         //X / Y axis
          $('#direction').innerHTML = (acceleration.x > 0 ? 'Turned left' : 'Turned right');
