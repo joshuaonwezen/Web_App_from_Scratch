c71122328bebca931bc33c2c4897327554b61c1d
Joshua Onwezen <joshua_onwezen@hotmail.com>
2016-02-18 21:34:32 +0100

    gravittyyyy

--- Opdrachten/js/eventHandling.js
+++ Opdrachten/js/eventHandling.js
@@ -21,38 +21,20 @@ var handler = {
             //soundcloud.getSoundcloudUser();
         });
         if (window.DeviceMotionEvent) {
-            window.addEventListener('devicemotion', deviceMotionHandler, false);
-        } else {
-            document.getElementById("dmEvent").innerHTML = "Not supported."
+            window.addEventListener('devicemotion', motionHandling.deviceMotionHandler, false);
         }
     },
 }
+var motionHandling = {
+    deviceMotionHandler: function (eventData) {
+        var gravity, xyz = "[X, Y, Z]";
 
-function deviceMotionHandler(eventData) {
-  var info, xyz = "[X, Y, Z]";
-
-  // Grab the acceleration from the results
-  var acceleration = eventData.acceleration;
-  info = xyz.replace("X", acceleration.x);
-  info = info.replace("Y", acceleration.y);
-  info = info.replace("Z", acceleration.z);
-  document.getElementById("moAccel").innerHTML = info;
-
-  // Grab the acceleration including gravity from the results
-  acceleration = eventData.accelerationIncludingGravity;
-  info = xyz.replace("X", acceleration.x);
-  info = info.replace("Y", acceleration.y);
-  info = info.replace("Z", acceleration.z);
-  document.getElementById("moAccelGrav").innerHTML = info;
-
-  // Grab the rotation rate from the results
-  var rotation = eventData.rotationRate;
-  info = xyz.replace("X", rotation.alpha);
-  info = info.replace("Y", rotation.beta);
-  info = info.replace("Z", rotation.gamma);
-  document.getElementById("moRotation").innerHTML = info;
-
-  // // Grab the refresh interval from the results
-  info = eventData.interval;
-  document.getElementById("moInterval").innerHTML = info;       
+        // Grab the acceleration including gravity from the results
+        var acceleration = eventData.accelerationIncludingGravity;
+        info = xyz.replace("X", acceleration.x);
+        info = info.replace("Y", acceleration.y);
+        info = info.replace("Z", acceleration.z);
+        document.getElementById("gravity").innerHTML = gravity;
+    },
 }
+
