273ceae3eb68aea869e7241e35497691fcf3d3c2
Joshua Onwezen <joshua_onwezen@hotmail.com>
2016-02-19 13:15:33 +0100

    small change

--- Opdrachten/js/templating.js
+++ Opdrachten/js/templating.js
@@ -41,8 +41,8 @@ var template = {
         obj.innerHTML += '<iframe width="400" height="300" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/' + data.id + '&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>';
 
         for (var i = 0; i < data.tracks.length; i++) {
-            obj.innerHTML += '<div id="trackinfo-'+data.tracks[i].id+'" class="trackinfo"><label>Track: </label><span class="title"></span><br><br><label>Link: </label><span class="permalink_url"></span><br><br><label>Favorites: </label><span class="favoritings_count"></span></div><br><br>';
-            template.detailSectionHandler('#trackinfo-'+data.tracks[i].id, data.tracks[i]);
+                obj.innerHTML += '<div id="trackinfo-' + data.tracks[i].id + '" class="trackinfo"><span class="title"></span><br><br><label>Link: </label><span class="permalink_url"></span><br><br><label>Favorites: </label><span class="favoritings_count"></span></div><br><br>';
+                template.detailSectionHandler('#trackinfo-' + data.tracks[i].id, data.tracks[i]);
         }
         window.onload(loader.hide());
 
